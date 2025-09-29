63b3d4cf07bdcbd9b847fd81cd504b973db754f4
elya <vavina.elvira@gmail.com>
2025-09-30 01:30:03 +0300

    change HTTPBasic -> OAuth2 (with no JWT yet)

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,24 +8,38 @@ from app.models.user import User
 from app.schemas import CreateUser
 from app.backend.db_depends import get_db
 
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-security = HTTPBasic()
 
-async def get_current_username(db: Annotated[AsyncSession, Depends(get_db)], credentials: HTTPBasicCredentials = Depends(security)):
-    user = await db.scalar(select(User).where(User.username == credentials.username))
-    if not user or not bcrypt_context.verify(credentials.password, user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+@router.post('/token')
+async def login(db: Annotated[AsyncSession, Depends(get_db)], form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = await authenticate_user(db, form_data.username, form_data.password)
+    return {
+        'access_token': user.username,
+        'token_type': 'bearer'
+    }
+
+
+async def authenticate_user(db: Annotated[AsyncSession, Depends(get_db)], username: str, password: str):
+    user = await db.scalar(select(User).where(User.username == username))
+    if not user or not bcrypt_context.verify(password, user.hashed_password) or user.is_active == False:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     return user
 
-
-@router.get('/users/me')
-async def read_current_user(user: User = Depends(get_current_username)):
-    return {'User': user}
-
+@router.get('/read_current_user')
+async def read_current_user(user: str = Depends(oauth2_scheme)):  # go to /token and return 'access_token' value
+    return user
+# when valid username and password are entered to oauth form,
+# header "Authorization: Bearer {user [token will be here]}" is added to request
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 async def create_user(db: Annotated[AsyncSession, Depends(get_db)], create_user: CreateUser):
